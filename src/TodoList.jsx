04eb2f7b69e0356ac251b0cdec390f534b8af8fc
unknown <you@example.comrenzoromero350@gmail.com>
2026-07-10 18:08:33 -0700

    Added submission form and seperated components

---
+++ src/TodoList.jsx
@@ -0,0 +1,20 @@
+const todoList = [
+  {id: 1, title: "review resources"},
+  {id: 2, title: "take notes"},
+  {id: 3, title: "take more notes"},
+  {id: 4, title: "code out app"}
+];
+
+function TodoList() {
+  return (
+    <>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default TodoList;
