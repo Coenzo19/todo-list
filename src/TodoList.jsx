be0477469074eca778886e5f3c9159422dc49d9b
unknown <you@example.comrenzoromero350@gmail.com>
2026-07-15 21:33:01 -0700

    updated Id

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,7 @@
 const todoList = [
   {id: 1, title: "review resources"},
   {id: 2, title: "take notes"},
-  {id: 4, title: "code out app"}
+  {id: 3, title: "code out app"}
 ];
 
 function TodoList() {
