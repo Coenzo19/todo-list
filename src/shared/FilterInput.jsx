a61755f19ea2d2eefe6b089ded20986732812907
unknown <you@example.comrenzoromero350@gmail.com>
2026-08-24 18:30:15 -0700

    changedText

--- src/shared/FilterInput.jsx
+++ src/shared/FilterInput.jsx
@@ -1,7 +1,7 @@
 export default function FilterInput({filterTerm, onFilterChange}) {
   return (
     <div>
-      <label htmlFor="filterInput">Search Todos</label>
+      <label htmlFor="filterInput">Search todos</label>
       <input
         type="text"
         id="filterInput"
