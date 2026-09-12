3edde8cadee10644d0c63180407ef53424df14d5
unknown <you@example.comrenzoromero350@gmail.com>
2026-09-12 12:45:31 -0700

    updated NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,6 +1,7 @@
-import {Link} from 'react-router'
-
+import {Link} from "react-router";
+import {useAuth} from "../contexts/AuthContext";
 export default function NotFoundPage() {
+  const {isAuthenticated} = useAuth();
   return (
     <div>
       <h2>404: Not Found</h2>
@@ -10,7 +11,17 @@ export default function NotFoundPage() {
       <Link className="linkButton" to="/about">
         About
       </Link>
-  
+
+      {isAuthenticated && (
+        <>
+          <Link className="linkButton" to="/profile">
+            Profile
+          </Link>
+          <Link className="linkButton" to="/todos">
+            Todos
+          </Link>
+        </>
+      )}
     </div>
   );
 }
